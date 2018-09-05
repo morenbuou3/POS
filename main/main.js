12a5ce43310d25e421678f23844a81900f4739e7
Jiahao Wang <jhwang@CNJiahao.local>
2018-09-05 23:55:20 +0800

    main/main.js

--- main/main.js
+++ main/main.js
@@ -1,5 +1,13 @@
-module.exports = function printInventory(inputs) {
-    console.log("Debug Info");
+const printProduct = require('../main/printProduct')
+const printPromotions = require('../main/printPromotions')
+const printTotal = require('../main/printTotal')
 
-    return 'Hello World!';
+module.exports = function printInventory(inputs) {
+    var result = '';
+
+    result += printProduct(inputs);
+    result += printPromotions(inputs);
+    result += printTotal(inputs);
+
+    console.log(result);
 };
